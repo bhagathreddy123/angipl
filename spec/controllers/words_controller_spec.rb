f21fdce4e8991f53a5de25c2275690b377cec302
Bhagath Reddy <katta04.bhagath2gmail.com>
2019-07-26 07:07:17 +0530

    added basic controller spec

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,11 +2,31 @@ require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
+
+  context 'when no words are assigned' do
+   it 'assigns @words' do
+      get :index
+      expect(assigns(:words)).to eq([])
+    end
+  end
+  
+  context 'when some words are assigned' do
+    it 'assigns @words' do
+      word = Word.create
+      get :index
+      expect(assigns(:words)).to eq([word])
+    end
+  end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
 
 end
